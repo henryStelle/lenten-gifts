67b11d16817d9b344884a224650ccc1cd9aaa949
Henry Stelle <21stellehenry@bprep.org>
2022-02-04 20:50:07 -0500

    add mui and delete starter code

---
+++ pages/_app.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import theme from "../utils/theme";
+import createEmotionCache from "../utils/createEmotionCache";
+import NavBar from "../components/NavBar";
+import { Toolbar } from "@mui/material";
+import { AppProps } from "next/app";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(
+  props: AppProps & { emotionCache: EmotionCache }
+) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <NavBar />
+        <Toolbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+}
---
+++ utils/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: { main: "#3f51b5" },
+    secondary: { main: "#fff" },
+  },
+});
+
+export default theme;
